4ae6eee342a93a2415bef7426257d5556e9a7c96
D Pro <dmytro.proskurnia@gmail.com>
2016-03-27 18:11:48 +0300

    first commit

---
+++ src/main/java/com/soundconnect/AuthController.java
@@ -0,0 +1,15 @@
+package com.soundconnect;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;    
+  
+@Controller  
+public class AuthController {  
+   
+	@RequestMapping("/auth")  
+	public String hello(Model model) {  
+		model.addAttribute("message", "This is data from Controller");
+		return "auth";  
+	}  
+} 
