fe68726269775bece97c82980ee1c2a6fee6aab3
Alex <alex.frankiv13@gmail.com>
2016-04-19 16:39:14 +0300

    fixed POST; got trouble with main.jsp

--- src/main/java/com/soundconnect/Controllers/PlayerController.java
+++ src/main/java/com/soundconnect/Controllers/PlayerController.java
@@ -22,7 +22,7 @@ public class PlayerController {
 	
 	@RequestMapping(path = "/content", method = RequestMethod.POST)
 	public String getPlaying(Model model, HttpServletRequest request) {
-		long conferenceId = Long.parseLong((String) request.getSession().getAttribute("confId"));
+		long conferenceId = (Long) request.getSession().getAttribute("confId");
 		Conference conference = conferenceService.getConferenceById(conferenceId);
 		conference.getTracks();
 		model.addAttribute("time", Calendar.getCurrentTime()-conference.getSongStarted());
